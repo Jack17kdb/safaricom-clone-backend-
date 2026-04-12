ccc2deeaf379871f99ca2cbf8394e82d67f0f51a
Jack17kdb <jack@kali.Johnson.com>
2026-04-13 00:42:18 +0300

    safaricom clone app

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = (id, res) => {
 	res.cookie("token", token, {
 		maxAge: 7 * 24 * 60 * 60 * 1000,
 		httpOnly: true,
-		sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax',
+		sameSite: 'lax',
 		secure: process.env.NODE_ENV !== 'development',
 		path: '/'
 	});
